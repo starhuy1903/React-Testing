f52c7d97acbfd6bb1f78718d195e60aa7a0ee64b
Huy Nguyen <huykst137@gmail.com>
2022-11-08 17:02:03 +0700

    Test with ReactDOM

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -11,23 +11,46 @@ import Counter from '../../components/counter'
 // Luckily, it's handled for you by React Testing Library :)
 global.IS_REACT_ACT_ENVIRONMENT = true
 
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
+  const div = document.createElement('div')
+  document.body.append(div)
+  // ReactDOM.render(<Counter/>, div);
   // 🐨 use createRoot to render the <Counter /> to the div
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
   // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
+  const [decrement, increment] = div.querySelectorAll('button')
   // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
+  const message = div.firstChild.querySelector('div')
   //
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
+  const incrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  // act(() => increment.click())
+  // increment.dispatchEvent(incrementClickEvent)
+  act(() => increment.dispatchEvent(incrementClickEvent))
+
   // 🐨 click the increment button (💰 act(() => increment.click()))
+  expect(message.textContent).toBe('Current count: 1')
   // 🐨 assert the message.textContent
+  const decrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  // act(() => decrement.click())
+  act(() => decrement.dispatchEvent(decrementClickEvent))
   // 🐨 click the decrement button (💰 act(() => decrement.click()))
   // 🐨 assert the message.textContent
-  //
+  expect(message.textContent).toBe('Current count: 0')
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
 })
