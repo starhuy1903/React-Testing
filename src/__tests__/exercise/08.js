7f3828001dcd5ebe7506da84755914185406c075
Huy Nguyen <huykst137@gmail.com>
2022-11-09 14:12:48 +0700

    Fake component to test Custom Hook

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -3,38 +3,22 @@
 
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
-
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-function UseCounterHookExample() {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div>Current count: {count}</div>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Increment</button>
-    </div>
-  )
-}
+import {act} from 'react-dom/test-utils'
 
 test('exposes the count and increment/decrement functions', async () => {
-  // 🐨 render the component
-  render(<UseCounterHookExample />)
-  // 🐨 get the elements you need using screen
-  const increment = screen.getByRole('button', {name: /increment/i})
-  const decrement = screen.getByRole('button', {name: /decrement/i})
-  const message = screen.getByText(/current count/i)
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
-  expect(message).toHaveTextContent('Current count: 0')
-  await userEvent.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
-  await userEvent.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
+  let result
+  function TestComponent() {
+    result = useCounter()
+    return null
+  }
+
+  render(<TestComponent />)
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
